77beb89d85b318606361098b6b314f6c40d496ce
AMAZINGGALAXYCODER <72047370+AMAZINGGALAXYCODER@users.noreply.github.com>
2021-01-03 13:34:20 -0500

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,23 @@
+class Box extends BaseClass {
+  constructor(x,y,width,height){
+    super(x,y,width,height);
+    this.Visiblity = 255;
+  }
+
+ display(){
+   console.log(this.body.speed);
+   if(this.body.speed < 5){
+    super.display();
+   }
+   else{
+     World.remove(world, this.body);
+     push();
+     this.Visiblity = this.Visiblity - 5;
+     pop();
+   }
+   
+ }
+
+
+
+};
